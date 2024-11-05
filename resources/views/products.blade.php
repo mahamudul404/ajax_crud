c8a1c0524163d54451b5420d120b34a94cf77e4a
Your New Name <mahamudul408432@gmail.com>
2024-11-05 12:59:19 +0600

    add modal

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -16,6 +16,7 @@
             <div class="col-md-2"></div>
             <div class="col-md-8">
                 <h2 class="text-center my-5">Laravel 11 Ajax Crud</h2>
+                <a class="btn btn-success my-3" data-bs-toggle="modal" data-bs-target="#exampleModal" href="">Add Product</a>
                 <div class="table-data">
 
                     <table class="table table-bordered">
@@ -49,19 +50,8 @@
     </div>
 
 
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-
-    <script>
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-    </script>
+@include('add_products_modal')
+@include('products_js')
 
 </body>
 
