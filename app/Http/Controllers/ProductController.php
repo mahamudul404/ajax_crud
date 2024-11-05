70643d7fd0de9a28ef8734312345eb7f68365f76
Your New Name <mahamudul408432@gmail.com>
2024-11-05 18:49:32 +0600

    show database data in view page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,8 @@ class ProductController extends Controller
 {
     public function index()
     {
-        return view('products');
+        $products = Product::all();
+        return view('products', compact('products'));
     }
 
     public function addProduct(Request $request)
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,8 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Ajax Crud</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 </head>
 
 <body>
@@ -16,7 +18,8 @@
             <div class="col-md-2"></div>
             <div class="col-md-8">
                 <h2 class="text-center my-5">Laravel 11 Ajax Crud</h2>
-                <a class="btn btn-success my-3" data-bs-toggle="modal" data-bs-target="#addModal" href="">Add Product</a>
+                <a class="btn btn-success my-3" data-bs-toggle="modal" data-bs-target="#addModal" href="">Add
+                    Product</a>
                 <div class="table-data">
 
                     <table class="table table-bordered">
@@ -29,16 +32,17 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="text-center">
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td class="text-center">
-                                    <a class="btn btn-primary " href=""> <i class="las la-edit"></i></a>
-                                    <a class="btn btn-danger" href=""> <i class="las la-trash"></i></a>
-                                </td>
-                            </tr>
-
+                            @foreach ($products as $product)
+                                <tr class="text-center">
+                                    <th scope="row"> {{ $product->id }} </th>
+                                    <td> {{ $product->name }} </td>
+                                    <td>  {{ $product->price }}</td>
+                                    <td class="text-center">
+                                        <a class="btn btn-primary " href=""> <i class="las la-edit"></i></a>
+                                        <a class="btn btn-danger" href=""> <i class="las la-trash"></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
 
@@ -50,8 +54,8 @@
     </div>
 
 
-@include('add_products_modal')
-@include('products_js')
+    @include('add_products_modal')
+    @include('products_js')
 
 </body>
 
