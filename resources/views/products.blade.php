450cf6326517e6018d2ba641dda797de38052761
Your New Name <mahamudul408432@gmail.com>
2024-11-06 13:23:31 +0600

    edit modal add

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -39,7 +39,10 @@
                                     <td> {{ $product->name }} </td>
                                     <td> {{ $product->price }}</td>
                                     <td class="text-center">
-                                        <a class="btn btn-primary " href=""> <i class="las la-edit"></i></a>
+                                        <a class="btn btn-primary " data-bs-toggle="modal" data-bs-target="#updateModal"
+                                            data-id="{{ $product->id }}" data-name="{{ $product->name }}"
+                                            data-price="{{ $product->price }}">
+                                            <i class="las la-edit"></i></a>
                                         <a class="btn btn-danger" href=""> <i class="las la-trash"></i></a>
                                     </td>
                                 </tr>
@@ -59,6 +62,7 @@
 
 
     @include('add_products_modal')
+    @include('update_products_modal')
     @include('products_js')
 
 </body>
---
+++ resources/views/update_products_modal.blade.php
@@ -0,0 +1,32 @@
+<!-- Modal -->
+<div class="modal fade" id="updateModal" tabindex="-1" aria-labelledby="updateModalLabel" aria-hidden="true">
+    <form action=" {{ route('add.product') }} " method="POST" id="updateProduct">
+        @csrf
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="addModalLabel">Update Product</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group m-2">
+                        <label for="name">Product Name</label>
+                        <input type="text" name="name" id="name" class="form-control"
+                            placeholder="Enter Product Name">
+                    </div>
+
+                    <div class="form-group m-2">
+                        <label for="price">Product Price</label>
+                        <input type="text" name="price" id="price" class="form-control"
+                            placeholder="Enter Product Price">
+                    </div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary  add_product" id="saveProduct">Update product</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
